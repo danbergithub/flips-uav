1eb7f4b3651601504aab233a4dc23de88aa108a3
robert.eunice <robert.eunice@0659fb78-53e4-11de-83c8-e5b33934001f>
2010-02-04 21:57:03 +0000

    Added example1.uav.asm for the MatrixPilot target.

---
+++ src/targets/matrixpilot/example1.uav.asm
@@ -0,0 +1,20 @@
+CMD 1                    // TAKEOFF / Command #1
+POS   X FIX -84.387982   // ATLANTA / 84.387982 W Longitude
+POS   Y FIX 33.748995    // ATLANTA / 33.748995 N Latitude
+POS   Z FIX -50.0        // 50.0 m (164.04199475065616 ft) Altitude
+FLY                      // Execute
+CMD 0                    // NORMAL / Command #0
+POS   X FIX -74.005973   // NEWYORK / 74.005973 W Longitude
+POS   Y FIX 40.714269    // NEWYORK / 40.714269 N Latitude
+FLY                      // Execute
+POS   Z FIX -304.8       // 304.8 m (1000.0 ft) Altitude
+FLY                      // Execute
+CMD 2                    // INVERTED / Command #2
+POS   X FIX -0.126708    // LONDON / 0.126708 W Longitude
+POS   Y FIX 51.00652     // LONDON / 51.00652 N Latitude
+FLY                      // Execute
+POS   X FIX -84.387982   // ATLANTA / 84.387982 W Longitude
+POS   Y FIX 33.748995    // ATLANTA / 33.748995 N Latitude
+POS   Z FIX -76.2        // 76.2 m (250.0 ft) Altitude
+FLY                      // Execute
+CMD 128                  // LAND / Command #128
