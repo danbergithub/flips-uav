885c57fe01bb6b9504999839df21659c408f6dad
robert.eunice <robert.eunice@0659fb78-53e4-11de-83c8-e5b33934001f>
2010-02-05 04:39:29 +0000

    Updated MatrixPilot example1.uav to include the example course from waypoints.h

--- src/targets/matrixpilot/example1.uav.asm
+++ src/targets/matrixpilot/example1.uav.asm
@@ -17,4 +17,24 @@ POS   X GEO -84.387982   // ATLANTA / 84.387982 W Longitude
 POS   Y GEO 33.748995    // ATLANTA / 33.748995 N Latitude
 POS   Z FIX -76.2        // 76.2 m (250.0 ft) Altitude
 FLY                      // Execute
+CMD 0                    // NORMAL / Command #0
+POS   X FIX 100.0        // SECORNER / 100.0 m (328.0839895013123 ft) E Distance
+POS   Y FIX 0.0          // SECORNER / 0.0 m (0.0 ft) N Distance
+POS   Z FIX -75.0        // 75.0 m (246.06299212598424 ft) Altitude
+FLY                      // Execute
+POS   X FIX 100.0        // NECORNER / 100.0 m (328.0839895013123 ft) E Distance
+POS   Y FIX 100.0        // NECORNER / 100.0 m (328.0839895013123 ft) N Distance
+FLY                      // Execute
+CMD 2                    // INVERTED / Command #2
+POS   X FIX 0.0          // NWCORNER / 0.0 m (0.0 ft) E Distance
+POS   Y FIX 100.0        // NWCORNER / 100.0 m (328.0839895013123 ft) N Distance
+FLY                      // Execute
+CMD 0                    // NORMAL / Command #0
+POS   X FIX 0.0          // SWCORNER / 0.0 m (0.0 ft) E Distance
+POS   Y FIX 0.0          // SWCORNER / 0.0 m (0.0 ft) N Distance
+FLY                      // Execute
+CMD 64                   // LOITER / Command #64
+POS   X FIX 50.0         // 50.0 m (164.04199475065616 ft) E Distance
+POS   Y FIX 50.0         // 50.0 m (164.04199475065616 ft) N Distance
+FLY                      // Execute
 CMD 128                  // LAND / Command #128
