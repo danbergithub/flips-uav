d7694d27466fe7e4ff4b5b33e66737b6ad1578b4
robert.eunice <robert.eunice@0659fb78-53e4-11de-83c8-e5b33934001f>
2010-08-26 00:25:46 +0000

    Updated asm2kml's flipsCompiler.java to accept input from a filename argument as well as stdin.

--- src/targets/kml/java/flipsCompiler.java
+++ src/targets/kml/java/flipsCompiler.java
@@ -1,9 +1,6 @@
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.tree.CommonTree;
@@ -12,30 +9,32 @@ import org.antlr.runtime.tree.CommonTreeNodeStream;
 public class flipsCompiler {
   public static void main(String args[]) throws
                                          Exception {
-    compile(args[0]);
+    CharStream input = null;
+    
+    // Select an input stream
+    if (args.length > 0) {
+      // Read file
+      input = new ANTLRFileStream(args[0]);
+    }
+    else {
+      // Read stdin
+      input = new ANTLRInputStream(System.in);
+    }
+    
+    // Print the compiled result
+    System.out.println(compile(input));
   }
   
-  public static void compile(String filename) throws
-                                              Exception {
-    // File Input
-    CharStream input = new ANTLRFileStream(filename);
-    
+  public static String compile(CharStream input) throws
+                                                 Exception {
     // Assembly Lexer
-    flipsTargetKmlLexer lex = new flipsTargetKmlLexer(input);
-    CommonTokenStream tokens = new CommonTokenStream(lex);
+    flipsTargetKmlLexer lexer = new flipsTargetKmlLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
     
     // Assembly Parser and Code Generator
     flipsTargetKmlParser parser = new flipsTargetKmlParser(tokens);
     parser.flightPlan();
     
-    System.out.println(parser.output.toString());
-    
-    // KML Output
-    try {
-      BufferedWriter channel = new BufferedWriter(new FileWriter(filename.replace(".asm",".kml"))); 
-      channel.write(parser.output.toString());
-      channel.close();
-    }
-    catch (IOException e) {}
+    return parser.output.toString();
   }
 }
